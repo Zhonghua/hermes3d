4d4b0e1d6137d80625cd59c4d0042a50f4a996fb
Ondrej Certik <ondrej@certik.cz>
2009-01-19 23:00:58 -0800

    Fix the FindUMFPACK.cmake file to find umfpack on Debian

--- cmake/FindUMFPACK.cmake
+++ cmake/FindUMFPACK.cmake
@@ -3,8 +3,8 @@
 #
 
 #FIND_PATH(UFCONFIG_INCLUDE_DIR UFconfig.h /usr/include/ /usr/local/include/UFconfig ${UFCONFIG_ROOT}/Include)
-FIND_PATH(UMFPACK_INCLUDE_DIR umfpack.h ${UMFPACK_ROOT}/Include /usr/include /usr/include/umfpack /usr/local/include/UMFPACK)
-FIND_PATH(AMD_INCLUDE_DIR amd.h ${AMD_ROOT}/Include /usr/include /usr/local/include/AMD)
+FIND_PATH(UMFPACK_INCLUDE_DIR umfpack.h ${UMFPACK_ROOT}/Include /usr/include /usr/include/umfpack /usr/local/include/UMFPACK /usr/include/suitesparse)
+FIND_PATH(AMD_INCLUDE_DIR amd.h ${AMD_ROOT}/Include /usr/include /usr/local/include/AMD /usr/include/suitesparse)
 
 FIND_LIBRARY(UMFPACK_LIBRARY umfpack ${UMFPACK_ROOT}/Lib /usr/lib /usr/local/lib/UMFPACK) 
 FIND_LIBRARY(AMD_LIBRARY amd ${AMD_ROOT}/Lib /usr/lib /usr/local/lib/AMD)

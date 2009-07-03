01b76fb9a8156b58d2a9c415a27b85587f2fbdda
David Andrs <andrsd@gmail.com>
2009-07-03 13:23:50 +0200

    Integrals updated to new-forms way

--- src/integrals/h1.h
+++ src/integrals/h1.h
@@ -26,307 +26,59 @@
 #include <common/error.h>
 #include <common/callstack.h>
 
-/// @defgroup h1intergrals H1 intergals
-
-#define H1_INTEGRATE_EXPRESSION(exp) \
-	double result = 0.0; \
-	QuadPt3D *pt = quad->get_points(o); \
-	int np = quad->get_num_points(o); \
-	if (ru->is_jacobian_const()) { \
-		for (int i = 0; i < np; i++) \
-			result += pt[i].w * (exp); \
-		result *= ru->get_const_jacobian(); \
-	} \
-	else { \
-		double *jac = ru->get_jacobian(o); \
-		for (int i = 0; i < np; i++) \
-			result += pt[i].w * jac[i] * (exp); \
-	}
-
-
-#define H1_INTEGRATE_DD_EXPRESSION(exp) \
-	double result = 0.0; \
-	{ QuadPt3D *pt = quad->get_points(o); \
-	int np = quad->get_num_points(o); \
-	double3x3 *mv, *mu; \
-	if (ru->is_jacobian_const()) { \
-		mu = ru->get_const_inv_ref_map(); \
-		mv = rv->get_const_inv_ref_map(); \
-		for (int i = 0; i < np; i++) \
-			result += pt[i].w * (exp); \
-		result *= ru->get_const_jacobian(); \
-	} \
-	else { \
-		mu = ru->get_inv_ref_map(o); \
-		mv = rv->get_inv_ref_map(o); \
-		double *jac = ru->get_jacobian(o); \
-		for (int i = 0; i < np; i++, mu++, mv++) \
-			result += pt[i].w * jac[i] * (exp); \
-	}}
-
-
-#define H1_INTEGRATE_SURF_EXPRESSION(exp) \
-	scalar result = 0.0; \
-	{ int np = quad->get_face_num_points(fp->face, face_order); \
-	fp->space = fp->space_v; \
-	QuadPt3D *pt = quad->get_face_points(fp->face, face_order); \
-	if (rv->is_face_const_jacobian(fp->face)) { \
-		for (int i = 0; i < np; i++) \
-			result += pt[i].w * (exp); \
-		result *= rv->get_face_const_jacobian(fp->face); \
-	} \
-	else { \
-		double *jac = rv->get_face_jacobian(fp->face, face_order); \
-		for (int i = 0; i < np; i++) \
-			result += pt[i].w * jac[i] * (exp); \
-	}}
-
-// inverse matrix is already transposed!
-#define T_DUDX (dudx[i] * (*mu)[0][0] + dudy[i] * (*mu)[0][1] + dudz[i] * (*mu)[0][2])
-#define T_DUDY (dudx[i] * (*mu)[1][0] + dudy[i] * (*mu)[1][1] + dudz[i] * (*mu)[1][2])
-#define T_DUDZ (dudx[i] * (*mu)[2][0] + dudy[i] * (*mu)[2][1] + dudz[i] * (*mu)[2][2])
-#define T_DVDX (dvdx[i] * (*mv)[0][0] + dvdy[i] * (*mv)[0][1] + dvdz[i] * (*mv)[0][2])
-#define T_DVDY (dvdx[i] * (*mv)[1][0] + dvdy[i] * (*mv)[1][1] + dvdz[i] * (*mv)[1][2])
-#define T_DVDZ (dvdx[i] * (*mv)[2][0] + dvdy[i] * (*mv)[2][1] + dvdz[i] * (*mv)[2][2])
+/// @defgroup h1integrals H1 intergals
 
 /// Integral \u
 ///
-/// @ingroup h1intergrals
-inline scalar int_u(RealFunction *fu, RefMap *ru) {
-	Quad3D *quad = fu->get_quad();
-
-	order3_t o = fu->get_fn_order() + ru->get_inv_ref_order();
-	o.limit();
-	qorder_t qord = ELEM_QORDER(o);
-	fu->set_quad_order(qord);
-
-	double *uval = fu->get_fn_values();
-
-	H1_INTEGRATE_EXPRESSION(uval[i]);
+/// @ingroup h1integrals
+template<typename f_t, typename res_t>
+res_t int_u(int n, double *wt, fn_t<f_t> *u, geom_t<f_t> *e) {
+	_F_
+	res_t result = 0;
+	for (int i = 0; i < n; i++)
+		result += wt[i] * (u->fn[i]);
 	return result;
 }
 
-#define int_v(fv, rv) int_u(fv, rv)
-
+#define int_v(n, wt, u, e) int_u(n, wt, u, e)
 
 /// Integral \u \v
 ///
-/// @ingroup h1intergrals
-inline scalar int_u_v(RealFunction *fu, RealFunction *fv, RefMap *ru, RefMap *rv) {
+/// @ingroup h1integrals
+template<typename f_t, typename res_t>
+res_t int_u_v(int n, double *wt, fn_t<f_t> *u, fn_t<f_t> *v, geom_t<f_t> *e) {
 	_F_
-	Quad3D *quad = fu->get_quad();
-
-	order3_t o = fu->get_fn_order() + fv->get_fn_order() + ru->get_inv_ref_order();
-	o.limit();
-	qorder_t qord = ELEM_QORDER(o);
-	fu->set_quad_order(qord);
-	fv->set_quad_order(qord);
-
-	double *uval = fu->get_fn_values();
-	double *vval = fv->get_fn_values();
-
-	H1_INTEGRATE_EXPRESSION(uval[i] * vval[i]);
+	res_t result = 0;
+	for (int i = 0; i < n; i++)
+		result += wt[i] * (u->fn[i] * v->fn[i]);
 	return result;
 }
-
 
 /// Integral \F \u
 ///
-/// @ingroup h1intergrals
-inline scalar int_F_u(double (*F)(double x, double y, double z), RealFunction *fu, RefMap *ru) {
+/// @ingroup h1integrals
+template<typename f_t, typename res_t>
+res_t int_F_v(int n, double *wt, res_t (*F)(f_t x, f_t y, f_t z), fn_t<f_t> *v, geom_t<f_t> *e) {
 	_F_
-	Quad3D *quad = fu->get_quad();
-
-	order3_t o = fu->get_fn_order();
-	o.set_maximal();
-	qorder_t qord = ELEM_QORDER(o);
-	fu->set_quad_order(qord);
-
-	double *uval = fu->get_fn_values();
-	double *x = ru->get_phys_x(o);
-	double *y = ru->get_phys_y(o);
-	double *z = ru->get_phys_z(o);
-
-	H1_INTEGRATE_EXPRESSION(uval[i] * F(x[i], y[i], z[i]));
+	res_t result = 0;
+	for (int i = 0; i < n; i++)
+		result += wt[i] * (v->fn[i] * (*F)(e->x[i], e->y[i], e->z[i]));
 	return result;
 }
 
-#define int_F_v(F, fv, rv) int_F_u(F, fv, rv)
-
+#define int_F_v(n, wt, F, v, e) int_F_u(n, wt, F, v, e)
 
 /// Integral \grad u \grad v
 ///
-/// @ingroup h1intergrals
-inline scalar int_grad_u_grad_v(RealFunction *fu, RealFunction *fv, RefMap *ru, RefMap *rv) {
+/// @ingroup h1integrals
+template<typename f_t, typename res_t>
+res_t int_grad_u_grad_v(int n, double *wt, fn_t<f_t> *u, fn_t<f_t> *v, geom_t<f_t> *e) {
 	_F_
-	Quad3D *quad = fu->get_quad();
-
-	order3_t o = fu->get_fn_order() + fv->get_fn_order() + ru->get_inv_ref_order();
-	o.limit();
-	qorder_t qord = ELEM_QORDER(o);
-	fu->set_quad_order(qord);
-	fv->set_quad_order(qord);
-
-	double *dudx, *dudy, *dudz;
-	fu->get_dx_dy_dz_values(dudx, dudy, dudz);
-	double *dvdx, *dvdy, *dvdz;
-	fv->get_dx_dy_dz_values(dvdx, dvdy, dvdz);
-
-	H1_INTEGRATE_DD_EXPRESSION(T_DUDX * T_DVDX + T_DUDY * T_DVDY + T_DUDZ * T_DVDZ);
+	res_t result = 0;
+	for (int i = 0; i < n; i++)
+		result += wt[i] * (u->dx[i] * v->dx[i] + u->dy[i] * v->dy[i] + u->dz[i] * v->dz[i]);
 	return result;
 }
 
-// surface integrals //////////////////////////////////////////////////////////////////////////////
-
-/// Integral \v (surface)
-///
-/// @ingroup h1intergrals
-inline scalar surf_int_v(RealFunction *fv, RefMap *rv, FacePos *fp) {
-	_F_
-	Quad3D *quad = fv->get_quad();
-
-	order3_t order = fv->get_fn_order() + rv->get_inv_ref_order();
-	order.limit();
-	order2_t face_order = order.get_face_order(fp->face);
-	qorder_t qord = FACE_QORDER(fp->face, face_order);
-	fv->set_quad_order(qord, FN_VAL);
-
-	double *vval = fv->get_fn_values();
-
-	H1_INTEGRATE_SURF_EXPRESSION(vval[i]);
-	return result;
-}
-
-/// Integral \G \v (surface)
-///
-/// @ingroup h1intergrals
-inline scalar surf_int_G_v(RealFunction *fv, RefMap *rv, FacePos *fp) {
-	_F_
-	Quad3D *quad = fv->get_quad();
-
-	order3_t o = fv->get_fn_order();
-	o.set_maximal();
-	order2_t face_order = o.get_face_order(fp->face);
-	qorder_t qord = FACE_QORDER(fp->face, face_order);
-	fv->set_quad_order(qord, FN_VAL);
-
-	double *vval = fv->get_fn_values();
-	double *x = rv->get_face_phys_x(fp->face, face_order);
-	double *y = rv->get_face_phys_y(fp->face, face_order);
-	double *z = rv->get_face_phys_z(fp->face, face_order);
-
-	H1_INTEGRATE_SURF_EXPRESSION(vval[i] * fp->space->bc_value_callback_by_coord(fp->marker, x[i], y[i], z[i]));
-	return result;
-}
-
-/// Integral \u \v (surface)
-///
-/// @ingroup h1intergrals
-inline scalar surf_int_u_v(RealFunction *fu, RealFunction *fv, RefMap *ru, RefMap *rv, FacePos *fp) {
-	_F_
-	Quad3D *quad = fu->get_quad();
-
-	order3_t order = fu->get_fn_order() + fv->get_fn_order() + ru->get_inv_ref_order();
-	order.limit();
-	order2_t face_order = order.get_face_order(fp->face);
-	qorder_t qord = FACE_QORDER(fp->face, face_order);
-
-	fu->set_quad_order(qord, FN_VAL);
-	fv->set_quad_order(qord, FN_VAL);
-
-	double *uval = fu->get_fn_values();
-	double *vval = fv->get_fn_values();
-
-	H1_INTEGRATE_SURF_EXPRESSION(uval[i] * vval[i]);
-	return result;
-}
-
-//// error & norm integrals ////////////////////////////////////////////////////////////////////////
-
-// TODO: merge this and things in norm.cc
-
-template<typename T>
-inline double int_h1_error(Function<T> *fu, Function<T> *fv, RefMap *ru, RefMap *rv) {
-	_F_
-	Quad3D *quad = fu->get_quad();
-	Quad3D *quadv = fv->get_quad();
-	assert(quad == fv->get_quad());
-
-	order3_t o = fu->get_fn_order() + fv->get_fn_order() + ru->get_inv_ref_order();
-	o.limit();
-	qorder_t qord = ELEM_QORDER(o);
-	fu->set_quad_order(qord);
-	fv->set_quad_order(qord);
-
-	scalar *fnu = fu->get_fn_values();
-	scalar *fnv = fv->get_fn_values();
-
-	scalar *dudx, *dudy, *dudz, *dvdx, *dvdy, *dvdz;
-	fu->get_dx_dy_dz_values(dudx, dudy, dudz);
-	fv->get_dx_dy_dz_values(dvdx, dvdy, dvdz);
-
-	H1_INTEGRATE_EXPRESSION(sqr(fnu[i] - fnv[i]) + sqr(dudx[i] - dvdx[i]) + sqr(dudy[i] - dvdy[i]) + sqr(dudz[i] - dvdz[i]));
-	return result;
-}
-
-template<typename T>
-inline double int_h1_semi_error(Function<T> *fu, Function<T> *fv, RefMap *ru, RefMap *rv) {
-	_F_
-	Quad3D *quad = fu->get_quad();
-	Quad3D *quadv = fv->get_quad();
-	assert(quad == fv->get_quad());
-
-	order3_t o = fu->get_fn_order() + fv->get_fn_order() + ru->get_inv_ref_order();
-	o.limit();
-	qorder_t qord = ELEM_QORDER(o);
-	fu->set_quad_order(qord);
-	fv->set_quad_order(qord);
-
-	scalar *fnu = fu->get_fn_values();
-	scalar *fnv = fv->get_fn_values();
-
-	scalar *dudx, *dudy, *dudz, *dvdx, *dvdy, *dvdz;
-	fu->get_dx_dy_values(dudx, dudy, dudz);
-	fv->get_dx_dy_values(dvdx, dvdy, dvdz);
-
-	H1_INTEGRATE_EXPRESSION(sqr(dudx[i] - dvdx[i]) + sqr(dudy[i] - dvdy[i]) + sqr(dudz[i] - dvdz[i]));
-	return result;
-}
-
-template<typename T>
-inline double int_h1_norm(Function<T> *fu, RefMap *ru) {
-	_F_
-	Quad3D *quad = fu->get_quad();
-
-	order3_t o = fu->get_fn_order() + ru->get_inv_ref_order();
-	o.limit();
-	qorder_t qord = ELEM_QORDER(o);
-	fu->set_quad_order(qord);
-
-	scalar *fnu = fu->get_fn_values();
-	scalar *dudx, *dudy, *dudz;
-	fu->get_dx_dy_dz_values(dudx, dudy, dudz);
-
-	H1_INTEGRATE_EXPRESSION(sqr(fnu[i]) + sqr(dudx[i]) + sqr(dudy[i]) + sqr(dudz[i]));
-	return result;
-}
-
-template<typename T>
-inline double int_h1_seminorm(Function<T> *fu, RefMap *ru) {
-	_F_
-	Quad3D *quad = fu->get_quad();
-
-	order3_t o = fu->get_fn_order() + ru->get_inv_ref_order();
-	o.limit();
-	qorder_t qord = ELEM_QORDER(o);
-	fu->set_quad_order(qord);
-
-	scalar *fnu = fu->get_fn_values();
-	scalar *dudx, *dudy, *dudz;
-	fu->get_dx_dy_dz_values(dudx, dudy, dudy);
-
-	H1_INTEGRATE_EXPRESSION(sqr(dudx[i]) + sqr(dudy[i]) + sqr(dudz[i]));
-	return result;
-}
 
 #endif
--- src/integrals/hcurl.h
+++ src/integrals/hcurl.h
@@ -24,292 +24,71 @@
 #include <common/error.h>
 #include <common/callstack.h>
 
-/// @defgroup hcurlintergrals HCurl intergals
-
-#define HCURL_INTEGRATE_EXPRESSION(exp) \
-	scalar result = 0.0; \
-	QuadPt3D *pt = quad->get_points(o); \
-	int np = quad->get_num_points(o); \
-	double3x3 *mv, *mu; \
-	if (ru->is_jacobian_const()) { \
-		mu = ru->get_const_inv_ref_map(); \
-		mv = rv->get_const_inv_ref_map(); \
-		for (int i = 0; i < np; i++) \
-			result += pt[i].w * (exp); \
-		result *= ru->get_const_jacobian(); \
-	} \
-	else { \
-		mu = ru->get_inv_ref_map(o); \
-		mv = rv->get_inv_ref_map(o); \
-		double *jac = ru->get_jacobian(o); \
-		for (int i = 0; i < np; i++, mu++, mv++) \
-			result += pt[i].w * jac[i] * (exp); \
-	}
-
-#define HCURL_INTEGRATE_CURL_EXPRESSION(exp) \
-	scalar result = 0.0; \
-	QuadPt3D *pt = quad->get_points(o); \
-	int np = quad->get_num_points(o); \
-	double3x3 *mv, *mu; \
-	if (ru->is_jacobian_const()) { \
-		mu = ru->get_const_ref_map(); \
-		mv = rv->get_const_ref_map(); \
-		for (int i = 0; i < np; i++) \
-			result += pt[i].w * (exp); \
-		result /= ru->get_const_jacobian(); \
-	} \
-	else { \
-		mu = ru->get_ref_map(o); \
-		mv = rv->get_ref_map(o); \
-		double *jac = ru->get_jacobian(o); \
-		for (int i = 0; i < np; i++, mu++, mv++) \
-			result += pt[i].w  * (exp) / jac[i]; \
-	}
-
-#define HCURL_INTEGRATE_SURF_EXPRESSION(exp) \
-	scalar result = 0.0; \
-	int np = quad->get_face_num_points(fp->face, face_order); \
-	fp->space = fp->space_v; \
-	QuadPt3D *pt = quad->get_face_points(fp->face, face_order); \
-	Point3D *nu, *nv; \
-	double3x3 *mv, *mu; \
-	if (rv->is_face_const_jacobian(fp->face)) { \
-		nu = ru->get_face_const_normal(fp->face); \
-		nv = rv->get_face_const_normal(fp->face); \
-		mu = ru->get_face_const_inv_ref_map(fp->face); \
-		mv = rv->get_face_const_inv_ref_map(fp->face); \
-		for (int i = 0; i < np; i++) \
-			result += pt[i].w * (exp); \
-		result *= rv->get_face_const_jacobian(fp->face); \
-	} \
-	else { \
-		double *jac = rv->get_face_jacobian(fp->face, face_order); \
-		nu = ru->get_face_normal(fp->face, face_order); \
-		nv = rv->get_face_normal(fp->face, face_order); \
-		mu = ru->get_face_inv_ref_map(fp->face, face_order); \
-		mv = rv->get_face_inv_ref_map(fp->face, face_order); \
-		for (int i = 0; i < np; i++, nu++, nv++, mu++, mv++) \
-			result += pt[i].w * jac[i] * (exp); \
-	}
-
-
-#define U_CURL_0 (du2dy[i] - du1dz[i])
-#define U_CURL_1 (du0dz[i] - du2dx[i])
-#define U_CURL_2 (du1dx[i] - du0dy[i])
-
-#define V_CURL_0 (dv2dy[i] - dv1dz[i])
-#define V_CURL_1 (dv0dz[i] - dv2dx[i])
-#define V_CURL_2 (dv1dx[i] - dv0dy[i])
-
-#define T_U_CURL_0 (U_CURL_0 * (*mu)[0][0] + U_CURL_1 * (*mu)[0][1] + U_CURL_2 * (*mu)[0][2])
-#define T_U_CURL_1 (U_CURL_0 * (*mu)[1][0] + U_CURL_1 * (*mu)[1][1] + U_CURL_2 * (*mu)[1][2])
-#define T_U_CURL_2 (U_CURL_0 * (*mu)[2][0] + U_CURL_1 * (*mu)[2][1] + U_CURL_2 * (*mu)[2][2])
-
-#define T_V_CURL_0 (V_CURL_0 * (*mv)[0][0] + V_CURL_1 * (*mv)[0][1] + V_CURL_2 * (*mv)[0][2])
-#define T_V_CURL_1 (V_CURL_0 * (*mv)[1][0] + V_CURL_1 * (*mv)[1][1] + V_CURL_2 * (*mv)[1][2])
-#define T_V_CURL_2 (V_CURL_0 * (*mv)[2][0] + V_CURL_1 * (*mv)[2][1] + V_CURL_2 * (*mv)[2][2])
-
-#define T_U_0 (u0[i] * (*mu)[0][0] + u1[i] * (*mu)[0][1] + u2[i] * (*mu)[0][2])
-#define T_U_1 (u0[i] * (*mu)[1][0] + u1[i] * (*mu)[1][1] + u2[i] * (*mu)[1][2])
-#define T_U_2 (u0[i] * (*mu)[2][0] + u1[i] * (*mu)[2][1] + u2[i] * (*mu)[2][2])
-
-#define T_V_0 (v0[i] * (*mv)[0][0] + v1[i] * (*mv)[0][1] + v2[i] * (*mv)[0][2])
-#define T_V_1 (v0[i] * (*mv)[1][0] + v1[i] * (*mv)[1][1] + v2[i] * (*mv)[1][2])
-#define T_V_2 (v0[i] * (*mv)[2][0] + v1[i] * (*mv)[2][1] + v2[i] * (*mv)[2][2])
-
-// for surface integrals : unit normal x u
-#define N_X_U_0 ((*nu).y * T_U_2 - (*nu).z * T_U_1)
-#define N_X_U_1 ((*nu).z * T_U_0 - (*nu).x * T_U_2)
-#define N_X_U_2 ((*nu).x * T_U_1 - (*nu).y * T_U_0)
-
-// for surface integrals : unit normal x v
-#define N_X_V_0 ((*nv).y * T_V_2 - (*nv).z * T_V_1)
-#define N_X_V_1 ((*nv).z * T_V_0 - (*nv).x * T_V_2)
-#define N_X_V_2 ((*nv).x * T_V_1 - (*nv).y * T_V_0)
-
-// for surface integrals : unit normal x u x unit normal ... tangent projection of u
-#define N_X_U_X_N_0 (N_X_U_1 * (*nu).z - N_X_U_2 * (*nu).y)
-#define N_X_U_X_N_1 (N_X_U_2 * (*nu).x - N_X_U_0 * (*nu).z)
-#define N_X_U_X_N_2 (N_X_U_0 * (*nu).y - N_X_U_1 * (*nu).x)
-
-// for surface integrals : unit normal x v x unit normal ... tangent projection of v
-#define N_X_V_X_N_0 (N_X_V_1 * (*nv).z - N_X_V_2 * (*nv).y)
-#define N_X_V_X_N_1 (N_X_V_2 * (*nv).x - N_X_V_0 * (*nv).z)
-#define N_X_V_X_N_2 (N_X_V_0 * (*nv).y - N_X_V_1 * (*nv).x)
-
-/// Integral \u \v
+/// Calculate curl of a vector
 ///
-/// @ingroup hcurlintergrals
-inline scalar hcurl_int_u_v(RealFunction *fu, RealFunction *fv, RefMap *ru, RefMap *rv) {
+/// @param dx[in] - vector of x-derivatives
+/// @param dy[in] - vector of y-derivatives
+/// @param dz[in] - vector of z-derivatives
+/// @param curl[out] - will contain the calculated curl
+template<typename T>
+void calc_curl(T (&dx)[3], T (&dy)[3], T (&dz)[3], T (&curl)[3]) {
+	curl[0] = dy[2] - dz[1];
+	curl[1] = dz[0] - dx[2];
+	curl[2] = dx[1] - dy[0];
+}
+
+/// calculates N x EV x N
+///
+/// @param nx[in] - x-component of the normal
+/// @param ny[in] - y-component of the normal
+/// @param nz[in] - z-component of the normal
+/// @param ev[in] - vector to work with
+/// @param tpe[out] - will contain the calculated value
+template<typename S, typename T>
+void calc_tan_proj(S nx, S ny, S nz, T (&ev)[3], T (&tpe)[3]) {
+	T ne[3] = {
+		ny * ev[2] - nz * ev[1],
+		nz * ev[0] - nx * ev[2],
+		nx * ev[1] - ny * ev[0]
+	};
+
+	tpe[0] = nz * ne[1] - ny * ne[2];
+	tpe[1] = nx * ne[2] - nz * ne[0];
+	tpe[2] = ny * ne[0] - nx * ne[1];
+}
+
+/// @ingroup hcurlintegrals
+
+template<typename f_t, typename res_t>
+res_t hcurl_int_u_v(int n, double *wt, fn_t<f_t> *u, fn_t<f_t> *v, geom_t<f_t> *e) {
 	_F_
-	Quad3D *quad = fu->get_quad();
-
-	order3_t o = fu->get_fn_order() + fv->get_fn_order() + ru->get_inv_ref_order();
-	qorder_t qord = ELEM_QORDER(o);
-	fu->set_quad_order(qord);
-	fv->set_quad_order(qord);
-
-	double *u0 = fu->get_fn_values(0);
-	double *u1 = fu->get_fn_values(1);
-	double *u2 = fu->get_fn_values(2);
-	double *v0 = fv->get_fn_values(0);
-	double *v1 = fv->get_fn_values(1);
-	double *v2 = fv->get_fn_values(2);
-
-	HCURL_INTEGRATE_EXPRESSION(T_U_0 * T_V_0 + T_U_1 * T_V_1 + T_U_2 * T_V_2);
+	res_t result = 0;
+	for (int i = 0; i < n; i++)
+		result += wt[i] * (u->fn0[i] * v->fn0[i] + u->fn1[i] * v->fn1[i] + u->fn2[i] * v->fn2[i]);
 	return result;
 }
 
-/// Integral \curl u \curl v
-///
-/// @ingroup hcurlintergrals
-inline scalar hcurl_int_curl_u_curl_v(RealFunction *fu, RealFunction *fv, RefMap *ru, RefMap *rv) {
+template<typename f_t, typename res_t>
+res_t hcurl_int_curl_u_curl_v(int n, double *wt, fn_t<f_t> *u, fn_t<f_t> *v, geom_t<f_t> *e) {
 	_F_
-	Quad3D *quad = fu->get_quad();
-
-	order3_t o = fu->get_fn_order() + fv->get_fn_order() + ru->get_ref_order();
-	qorder_t qord = ELEM_QORDER(o);
-	fu->set_quad_order(qord);
-	fv->set_quad_order(qord);
-
-	double *du0dx, *du0dy, *du0dz, *du1dx, *du1dy, *du1dz, *du2dx, *du2dy, *du2dz;
-	double *dv0dx, *dv0dy, *dv0dz, *dv1dx, *dv1dy, *dv1dz, *dv2dx, *dv2dy, *dv2dz;
-
-	fu->get_dx_dy_dz_values(du0dx, du0dy, du0dz, 0);
-	fu->get_dx_dy_dz_values(du1dx, du1dy, du1dz, 1);
-	fu->get_dx_dy_dz_values(du2dx, du2dy, du2dz, 2);
-
-	fv->get_dx_dy_dz_values(dv0dx, dv0dy, dv0dz, 0);
-	fv->get_dx_dy_dz_values(dv1dx, dv1dy, dv1dz, 1);
-	fv->get_dx_dy_dz_values(dv2dx, dv2dy, dv2dz, 2);
-
-	HCURL_INTEGRATE_CURL_EXPRESSION(T_U_CURL_0 * T_V_CURL_0 + T_U_CURL_1 * T_V_CURL_1 + T_U_CURL_2 * T_V_CURL_2);
+	res_t result = 0;
+	for (int i = 0; i < n; i++)
+		result += wt[i] * (u->curl0[i] * v->curl0[i] + u->curl1[i] * v->curl1[i] + u->curl2[i] * v->curl2[i]);
 	return result;
 }
 
-/// Integral \F \u
+/// Integral \F \v
 ///
-/// @ingroup hcurlintergrals
-inline scalar hcurl_int_F_u(scalar3 &(*F)(double x, double y, double z), RealFunction *fu, RefMap *ru) {
+template<typename f_t, typename res_t>
+res_t hcurl_int_F_v(int n, double *wt, void (*F)(f_t, f_t, f_t, res_t (&)[3]), fn_t<f_t> *v, geom_t<f_t> *e) {
 	_F_
-	Quad3D *quad = fu->get_quad();
-
-	order3_t o = fu->get_fn_order();
-	o.set_maximal();
-	qorder_t qord = ELEM_QORDER(o);
-	fu->set_quad_order(qord);
-
-	double *u0 = fu->get_fn_values(0);
-	double *u1 = fu->get_fn_values(1);
-	double *u2 = fu->get_fn_values(2);
-
-	double *x = ru->get_phys_x(o);
-	double *y = ru->get_phys_y(o);
-	double *z = ru->get_phys_z(o);
-
-	scalar result = 0.0;
-	QuadPt3D *pt = quad->get_points(o);
-	int np = quad->get_num_points(o);
-	double3x3 *mu;
-	if (ru->is_jacobian_const()) {
-		mu = ru->get_const_inv_ref_map();
-		for (int i = 0; i < np; i++) {
-			scalar3 &f = F(x[i], y[i], z[i]);
-			result += pt[i].w * (T_U_0 * f[0] + T_U_1 * f[1] + T_U_2 * f[2]);
-		}
-		result *= ru->get_const_jacobian();
+	res_t result = 0;
+	res_t f[3];
+	for (int i = 0; i < n; i++) {
+		F(e->x[i], e->y[i], e->z[i], f);
+		result += wt[i] * (v->fn0[i] * f[0] + v->fn1[i] * f[1] + v->fn2[i] * f[2]);
 	}
-	else {
-		mu = ru->get_inv_ref_map(o);
-		double *jac = ru->get_jacobian(o);
-		for (int i = 0; i < np; i++, mu++) {
-			scalar3 &f = F(x[i], y[i], z[i]);
-			result += pt[i].w * jac[i] * (T_U_0 * f[0] + T_U_1 * f[1] + T_U_2 * f[2]);
-		}
-	}
-
 	return result;
 }
-
-#define hcurl_int_F_v(F, fv, rv) hcurl_int_F_u(F, fv, rv)
-
-
-// surface integrals //////////////////////////////////////////////////////////////////////////////
-
-/// Integral \G dot \v projection (surface)
-///
-/// @ingroup hcurlintergrals
-inline scalar hcurl_surf_int_G_v(RealFunction *fv, RefMap *rv, FacePos *fp) {
-	_F_
-	Quad3D *quad = fv->get_quad();
-
-	order3_t order = fv->get_fn_order();
-	order.set_maximal();
-	order2_t face_order = order.get_face_order(fp->face);
-	qorder_t qord = FACE_QORDER(fp->face, face_order);
-
-	fv->set_quad_order(qord, FN_VAL);
-
-	double *v0 = fv->get_fn_values(0);
-	double *v1 = fv->get_fn_values(1);
-	double *v2 = fv->get_fn_values(2);
-
-	double *x = rv->get_face_phys_x(fp->face, face_order);
-	double *y = rv->get_face_phys_y(fp->face, face_order);
-	double *z = rv->get_face_phys_z(fp->face, face_order);
-
-	scalar result = 0.0;
-	int np = quad->get_face_num_points(fp->face, face_order);
-	fp->space = fp->space_v;
-	QuadPt3D *pt = quad->get_face_points(fp->face, face_order);
-	Point3D *nv;
-	double3x3 *mv;
-	if (rv->is_face_const_jacobian(fp->face)) {
-		nv = rv->get_face_const_normal(fp->face);
-		mv = rv->get_face_const_inv_ref_map(fp->face);
-		for (int i = 0; i < np; i++) {
-			scalar3 &f = fp->space->bc_vec_value_callback_by_coord(fp->marker, x[i], y[i], z[i]);
-			result += pt[i].w * (N_X_V_X_N_0 * f[0] + N_X_V_X_N_1 * f[1] + N_X_V_X_N_2 * f[2]);
-		}
-		result *= rv->get_face_const_jacobian(fp->face);
-	}
-	else {
-		double *jac = rv->get_face_jacobian(fp->face, face_order);
-		nv = rv->get_face_normal(fp->face, face_order);
-		mv = rv->get_face_inv_ref_map(fp->face, face_order);
-		for (int i = 0; i < np; i++, nv++, mv++) {
-			scalar3 &f = fp->space->bc_vec_value_callback_by_coord(fp->marker, x[i], y[i], z[i]);
-			result += pt[i].w * jac[i] * (N_X_V_X_N_0 * f[0] + N_X_V_X_N_1 * f[1] + N_X_V_X_N_2 * f[2]);
-		}
-	}
-
-	return result;
-}
-
-/// Integral \u projection dot \v projection (surface)
-///
-/// @ingroup h1intergrals
-inline scalar hcurl_surf_int_u_v(RealFunction *fu, RealFunction *fv, RefMap *ru, RefMap *rv, FacePos *fp) {
-	_F_
-	Quad3D *quad = fu->get_quad();
-
-	order3_t order = fu->get_fn_order() + fv->get_fn_order() + ru->get_inv_ref_order();
-	order2_t face_order = order.get_face_order(fp->face);
-	qorder_t qord = FACE_QORDER(fp->face, face_order);
-
-	fu->set_quad_order(qord, FN_VAL);
-	fv->set_quad_order(qord, FN_VAL);
-
-	double *u0 = fu->get_fn_values(0);
-	double *u1 = fu->get_fn_values(1);
-	double *u2 = fu->get_fn_values(2);
-	double *v0 = fv->get_fn_values(0);
-	double *v1 = fv->get_fn_values(1);
-	double *v2 = fv->get_fn_values(2);
-
-	HCURL_INTEGRATE_SURF_EXPRESSION(N_X_U_X_N_0 * N_X_V_X_N_0 + N_X_U_X_N_1 * N_X_V_X_N_1 + N_X_U_X_N_2 * N_X_V_X_N_2);
-	return result;
-}
-
 
 #endif
